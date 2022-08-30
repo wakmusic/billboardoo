4b8fe310e7f796a77088fb22fc8da8e4a5fe82ca
Nian <44210830+frin0911@users.noreply.github.com>
2022-08-30 22:53:42 +0900

    헤더 로그인 버튼 추가

--- components/header.js
+++ components/header.js
@@ -1,10 +1,22 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import logo from '../images/billboardoo.png';
 import '../stylesheets/index.css'
+import axios from "axios";
 
 
 function Header(props) {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        async function get() {
+            const result = await axios.get(
+                `/api/auth`
+            );
+            setData(result.data);
+        }
+        get().then();
+    }, [props.type]);
+
     function openMenu() {
         const burger = document.getElementById('burger');
         const slide = document.getElementById('menu-slide');
@@ -18,6 +30,35 @@ function Header(props) {
         }
     }
 
+    function loadProfile() {
+        if (!data || data.data === 401) return <Link to="/login" className="login-btn">로그인</Link>;
+        switch (data.provider) {
+            case "naver":
+                return (
+                    <div className="profile">
+                        <img src={data._json.profile_image} alt="profile" className="profile-img"/>
+                        <span className="display-name">{data.displayName}</span>
+                    </div>
+                );
+            case "google":
+                return (
+                    <div className="profile">
+                        <img src={data._json.picture} alt="profile" className="profile-img"/>
+                        <span className="display-name">{data.displayName}</span>
+                    </div>
+                );
+            case "twitch":
+                return (
+                    <div className="profile">
+                        <img src={data.profile_image_url} alt="profile" className="profile-img"/>
+                        <span className="display-name">{data.display_name}</span>
+                    </div>
+                )
+            default:
+                return <></>
+        }
+    }
+
     return (
         <div className="header">
             <Link to="/">
@@ -28,9 +69,9 @@ function Header(props) {
                 <Link to="/albums" className="menu-item">ALBUMS</Link>
                 <Link to="/artists" className="menu-item">ARTISTS</Link>
                 <Link to="/news" className="menu-item">NEWS</Link>
-                <Link to="/credit" className="menu-item">CREDIT</Link>
-                <a href="https://waklon.billboardoo.com" target="_blank" rel="noreferrer"
-                   className="menu-item">WAKLON</a>
+                <div className="right">
+                    {loadProfile()}
+                </div>
             </div>
             <div className="mobile-menu">
                 <div className="mob-menu" id="mob-menu" onClick={openMenu}>
